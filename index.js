aa0e45ca84a541db75768808ce83aace98c34dcd
errijahi <76177230+errijahi@users.noreply.github.com>
2023-02-15 17:05:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ const app = express();
 import cors from "cors";
 
 const corsOptions = {
-  "origin": 'https://bookstore-react-4poq.onrender.com',
-  "credentials": true, 
-//   "origin": "*",
-  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-  "preflightContinue": false,
+  origin: 'https://bookstore-react-4poq.onrender.com',
+  credentials: true, 
+  //origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
 };
 
 app.use(cors(corsOptions));
