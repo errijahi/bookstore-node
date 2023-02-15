26da596ecd6d75f58be49380ef2fc66ddb60c6c8
errijahi <76177230+errijahi@users.noreply.github.com>
2023-02-15 12:28:36 +0100

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -44,6 +44,8 @@ export const login = (req,res) =>{
 
     res.cookie("access_token",token,{
         httpOnly:true,
+        sameSite:"none",
+        secure:true
     }).status(200).json(other)
     
     })
